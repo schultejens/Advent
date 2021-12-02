1ffdb6f392e3c513c7c0cc46c42c707bba3854d0
schultejens <jens.schulte@uni-oldenburg.de>
2021-12-02 10:14:09 +0100

    Advent2.1

---
+++ Advent2.1.py
@@ -0,0 +1,22 @@
+with open('input2.1.txt') as f:
+    inputlist = f.readlines()
+
+#Input konvertieren
+inputlist = list((map(lambda s: s.strip(), inputlist)))
+direction, amount = zip(*(s.split(' ') for s in inputlist))
+amount = list(map(int,amount))
+
+horizontal = 0
+depth = 0
+
+for i in range(0,len(inputlist)):
+    if direction[i] == 'forward':
+        horizontal += amount[i]
+    if direction[i] == 'up':
+        depth -= amount[i]
+    if direction[i] == 'down':
+        depth += amount[i]
+
+print(horizontal)
+print(depth)
+print(horizontal*depth)
