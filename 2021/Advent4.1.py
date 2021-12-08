a65145ebfb6abef45741d257f1c7ff097e0896ee
schultejens <jens.schulte@uni-oldenburg.de>
2021-12-08 19:38:02 +0100

    advent 4.1

--- 2021/Advent4.1.py
+++ 2021/Advent4.1.py
@@ -1,35 +1,49 @@
 from own_tools import get_strings
+import numpy as np
 
-#getBoards
 def readBoard(boardText):
     boards = []
     boardBuffer = []
     for row in boardText:
         if len(row) == 0:
-            boards.append(boardBuffer.pop())
+            boards.append(boardBuffer)
+            boardBuffer = []
         else:
             boardBuffer.append(list(map(int,row.split())))
-    boards.append(boardBuffer)
     return boards
 
 def checkPick(board,pick):
-     for row in range(len(board)):
+    for row in range(len(board)):
             for col in range(len(board)):
-                print(pick)
                 if board[row][col] == pick:
                    board[row][col] = 0
-        #[[number.replace(pick, '0') for number in row] for row in board]
+    return board
       
+def checkRowWin(board):
+    numpyArray = np.array(board)
+    return min(np.sum(numpyArray, axis=1))==0
+
+    
+def checkColWin(board):
+    numpyArray = np.array(board)
+    return min(np.sum(numpyArray, axis=0)) == 0
+
+def checkWin(board):
+    if(checkRowWin(board)):
+        return board
+    if(checkColWin(board)):
+        return board
+    return None
+          
 def picking(boards,picks):
     for pick in picks:
         for board in boards:
             checkPick(board, pick)
-
+            if(checkWin(board) is not None):
+                return board, pick
+                               
 boards = readBoard(get_strings("2021/input4_boards.txt"))
 picks = list(map(int,get_strings("2021/input4_picks.txt")[0].split(',')))
 
-print(boards[0])
-print(picks)
-picking(boards, picks)
-#print(boards)
-#print(picks)         
+winningboard = picking(boards, picks)
+print(np.sum(winningboard[0])*winningboard[1])    
