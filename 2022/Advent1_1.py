91443d1755f84288874112809aaf252470f66fad
schultejens <jens.schulte@uni-oldenburg.de>
2022-12-03 20:51:27 +0100

    Advent

---
+++ 2022/Advent1_1.py
@@ -0,0 +1,18 @@
+import numpy as np
+calories = []
+caloriebuffer = 0
+
+with open('2022/input1_1.txt') as f:
+    inputlist = f.readlines()
+
+for i in range(0,len(inputlist)):
+    if(inputlist[i] == '\n'):
+        calories.append(caloriebuffer)
+        caloriebuffer = 0
+        continue
+    caloriebuffer += int(inputlist[i])     
+    
+calories = np.array(calories)
+print(np.max(calories))
+calories2 = np.argsort(-calories,axis=0)[:3]
+print (calories[calories2[0]]+calories[calories2[1]]+calories[calories2[2]])
---
+++ 2022/Advent2_1.py
@@ -0,0 +1,21 @@
+with open('2022/input2_1.txt') as f:
+    inputlist = f.read().splitlines()
+sump1=0
+sump2=0
+#P1 
+dict = {
+    "A Y": 8,"B Z": 9,"C X": 7, #wins
+    "A X": 4,"B Y": 5,"C Z": 6, #draws
+    "X": 1,"Y":2,"Z":3 #loss
+}
+#P2 X Loose, Y Draw, Z Win
+dict2 = {"A X":3, "A Y":4, "A Z":8, 
+         "B X":1, "B Y":5, "B Z":9,
+         "C X":2, "C Y":6, "C Z":7
+         }
+#P1
+for i in range(0,len(inputlist)):
+    sump1+=dict.get(inputlist[i],dict.get(inputlist[i].split(' ')[1]))
+    sump2+=dict2.get(inputlist[i],dict.get(inputlist[i].split(' ')[1]))      
+print(sump1)
+print(sump2)
