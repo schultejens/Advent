55b7fcdf28a5591d449e8f4c4cdb38da6c59c5e9
schultejens <jens.schulte@uni-oldenburg.de>
2022-12-05 22:30:31 +0100

    Added DeepCopy

--- 2022/Advent5_1.py
+++ 2022/Advent5_1.py
@@ -1,6 +1,8 @@
 import numpy as np
 import time
 import re
+import copy
+
 start_time = time.time()
 
 stacksPassed = False
@@ -21,6 +23,7 @@ for i in range(0,len(inputlist)): #readStacks
           break        
 for i in range(len(stack)-1,-1,-1): #rearrange list-elements 
           stackArrange.append(stack[i].replace('    ',' ').split(' '))
+          
 stackArrange[0] = [value for value in stackArrange[0] if value!= '']
 stacky = np.array(stackArrange)
 stacky = stacky.T
@@ -29,7 +32,7 @@ stackylist=[]
 
 for row in stacky:
        stackylist.append([val for val in row.tolist() if val !='']) #remove ''
-stackylist2=stackylist
+stackylist2=copy.deepcopy(stackylist)
 
 for i in range(indexStart+1,len(inputlist)-1):
     steps = list(map(int,re.findall(r'\d+',inputlist[i]))) #get steps as numbers 0,1,2
@@ -48,4 +51,4 @@ for k in stackylist2:
       
 
 print("-- %s seconds --" % (time.time() - start_time))
-#-- 0.00500178337097168 seconds --
+#-- 0.003000974655151367 seconds --
